dd3f559dd8f7718cffabea7afe0beaf13e85c17f
samskalicky <samskalicky@478d732c-b2ee-11de-bed4-89e45268dd3d>
2009-10-15 23:07:34 +0000

    Added items remotely
    C:\Documents and Settings\sxs5464\Desktop\DSD\XOR

---
+++ XOR/Sigmoid.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:58:47 10/15/2009 
+-- Design Name: 
+-- Module Name:    Sigmoid - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Sigmoid is
+    Port ( X : in  STD_LOGIC;
+           Y : out  STD_LOGIC);
+end Sigmoid;
+
+architecture Behavioral of Sigmoid is
+
+begin
+
+
+end Behavioral;
+
