b57235c3befb828246e29cc76683537e1c5b5822
samskalicky <samskalicky@478d732c-b2ee-11de-bed4-89e45268dd3d>
2009-10-25 05:06:59 +0000

    initial version

---
+++ VHDL/Sigmoid2.vhd
@@ -0,0 +1,42 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:58:47 10/15/2009 
+-- Design Name: 
+-- Module Name:    Sigmoid - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Sigmoid is
+    Port ( X : in  STD_LOGIC_VECTOR(15 downto 0);
+           Y : out  STD_LOGIC_VECTOR(7 downto 0));
+end Sigmoid;
+
+architecture Behavioral of Sigmoid is
+	variable pow : Integer:=0;
+	variable previous : Boolean:=False;
+	variable converge : unsigned(7 downto 0):="
+begin
+	
+	
+end Behavioral;
