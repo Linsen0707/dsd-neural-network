f0193eb4fc9c3d9de25b10888c32d0bc8fba4684
samskalicky <samskalicky@478d732c-b2ee-11de-bed4-89e45268dd3d>
2009-10-26 03:13:05 +0000

    working version as of 11:12pm 10/25/09

---
+++ VHDL/multiply.vhd
@@ -0,0 +1,15 @@
+--beginning case
+s(0)=a(0)AND b(0)
+
+--end case
+s(15)=a(7)AND b(7)
+
+--normal case
+int p=cnt-1;
+for(int q=0; q<cnt; q++) {
+	--generate adders
+	Adder: FullAdder
+		port map(a(p),b(
+	--decrement counter
+	p=p-1;
+}
