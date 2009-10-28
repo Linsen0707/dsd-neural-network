a45e2d31036fde18045ddb29efc867b0979d9098
samskalicky <samskalicky@478d732c-b2ee-11de-bed4-89e45268dd3d>
2009-10-28 03:09:03 +0000

    new

--- VHDL/Sigmoid2.vhd
+++ VHDL/Sigmoid2.vhd
@@ -22,6 +22,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
+library floatfixlib;
+use floatfixlib.float_pkg.ALL;
+
 ---- Uncomment the following library declaration if instantiating
 ---- any Xilinx primitives in this code.
 --library UNISIM;
@@ -37,11 +40,29 @@ architecture Behavioral of Sigmoid is
 begin
   process (X)
   	 variable pow : Integer:=0;
-    variable previous,result : signed(15 downto 0);
-    variable converge : unsigned(7 downto 0):="00000001";
+    variable result,prev,exp : float32;
+    variable converge : float32:= to_float(0.1,32);
+    variable val,div : float32:=to_float(1.0,32);
 	begin
-	  while abs((previous-result)/result) < converge loop
-	    
+	  while abs((prev-result)/result) >= converge loop
+	     if pow = 0 then
+	      exp := to_float(1,32);
+	     else
+	       exp := val;
+	     end if;
+	     
+	     for i in 0 to pow-1 loop
+	       exp := exp * val;
+	     end loop;
+	     
+	     for i in pow downto 0 loop
+	       div := div * i;
+	     end loop;
+	     
+	     result := result + exp/div;
+	     
+	     prev := result;
+	     pow := pow + 1;
 	  end loop;
 	end process;
 end Behavioral;
