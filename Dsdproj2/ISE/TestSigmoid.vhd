6b5c180d5a2f6cee96d03bce90e8402e13881016
subquantum <subquantum@478d732c-b2ee-11de-bed4-89e45268dd3d>
2009-10-31 20:13:20 +0000

    Now the sigmoid works with our fixed-point numbers, caps out when the taylor series is a bad approximation, and does so instantly.

--- Dsdproj2/ISE/TestSigmoid.vhd
+++ Dsdproj2/ISE/TestSigmoid.vhd
@@ -17,6 +17,32 @@
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+entity TestSigGen is
+  Port ( x : out signed(15 downto 0);
+         v : out Integer);
+end TestSigGen;
+
+architecture B of TestSigGen is
+  begin
+    process
+      variable msb : std_logic;
+    begin                 
+--testing procedure
+     --loop through all input
+       for i in -16 to 16 loop
+          v <= i / 4;
+          msb := to_signed(i,8)(7);
+          x <= msb&msb&to_signed(i,8)&"000000";
+         wait for 10 ns; 
+       end loop;
+     end process;
+end B;
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -24,38 +50,28 @@ use IEEE.NUMERIC_STD.ALL;
 entity TestSigmoid is
 end TestSigmoid;
 
+
 architecture Behavioral of TestSigmoid is
     
-    component Sigmoid is
+    component Sigmoid3 is
       Port ( X : in  signed(15 downto 0);
              Y : out  unsigned(7 downto 0));
     end component;
+    component TestSigGen is
+        Port ( x : out signed(15 downto 0);
+         v : out Integer);
+  end component;
     
-    component Sigmoid2 is
-      Port ( X : in  signed(15 downto 0);
-             Y : out  unsigned(7 downto 0):="00000000");
-    end component;
-    
-    signal inputs : signed(15 downto 0):=to_signed(0,16);
-    signal output : unsigned(7 downto 0):="00000000";
-    signal value : Integer:=0;
-    signal error : STD_LOGIC := '0';
-    signal delay : Time := 10 ns;
+    signal inputs : signed(15 downto 0);
+    signal output : unsigned(7 downto 0);
     signal val : Integer;
     begin
         
     --Unit Under Test
-        UUT: Sigmoid2
+        UUT: Sigmoid3
            port map(inputs,output);
          
-        process
-        begin                 
-    --testing procedure
-           --loop through all input
-           for i in -7 to 7 loop
-              val<=i;
-              inputs<=to_signed(i,8)&"00000000";
-              wait for delay; 
-           end loop;
-       end process;
+         Gen: TestSigGen
+          port map(inputs, val);
+
 end Behavioral;
