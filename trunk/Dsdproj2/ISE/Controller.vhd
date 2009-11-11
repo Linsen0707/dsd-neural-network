7e11331dea79339d1fed826bb412aa078c30ad0b
samskalicky <samskalicky@478d732c-b2ee-11de-bed4-89e45268dd3d>
2009-11-11 21:52:01 +0000

    inital version of user ctrls

--- trunk/Dsdproj2/ISE/Controller.vhd
+++ trunk/Dsdproj2/ISE/Controller.vhd
@@ -23,29 +23,48 @@ use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 Entity Controller is
-    Port (inputs : in std_logic_vector( 1 downto 0 );
-          output : out std_logic_vector( 7 downto 0 ));
+    Port (inputs : in std_logic_vector( 1 downto 0 );     --input values from switches
+          mode : in boolean;
+          output : out std_logic_vector( 7 downto 0 ));   --output result for LEDs
 end Controller;
 
 architecture Behavioral of Controller is
 
   component Network is
-    Port (	testa : in  unsigned (7 downto 0);
-				testb : in  unsigned (7 downto 0);
-				classification : out  unsigned (7 downto 0);
-				lasterror : in signed(15 downto 0);
-				learnmode : in boolean);
+    Port (	testa : in  unsigned (7 downto 0);         --input value 0
+				testb : in  unsigned (7 downto 0);            --input value 1
+				classification : out  unsigned (7 downto 0);  --result from network
+				lasterror : in signed(15 downto 0);           --error from last calculation
+				learnmode : in boolean);                      --mode selection
   end component;
   
-  
+  component Input is
+		Port ( Input : in  STD_LOGIC;                 --input node 
+           Output : out  unsigned (7 downto 0));  --output vector (8 bit decimal)
+	end component;
+	
+	component ErrorCalc is
+      Port ( inputs : in std_logic_vector (1 downto 0);   --inputs from switches
+             result : in unsigned (7 downto 0);   --result from network
+             error : out signed (15 downto 0));           --result of error calculation (output)
+  end component;
 
   type conlevel is array(1 downto 0) of unsigned(7 downto 0);
-  signal netInputs : conlevel
+  signal netInputs : conlevel;
+  signal error : signed(15 downto 0);
+  signal result : unsigned(7 downto 0);
 begin
+  
+  --instantiate components
   in1: Input
 		port map(inputs(0),netInputs(0));
 		
 	in2: Input
 		port map(inputs(1),netInputs(1));
 		
+	net: Network
+	  port map(netInputs(0),netInputs(1),result, error, mode);
+		
+	err: ErrorCalc
+	  port map(inputs,result,error);
 end Behavioral;          
--- trunk/Dsdproj2/ISE/UserControls.vhd
+++ trunk/Dsdproj2/ISE/UserControls.vhd
@@ -24,17 +24,40 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 Entity UserController is
     Port ( sw : in STD_LOGIC_VECTOR(7 downto 0);
-           led : in STD_LOGIC_VECTOR(7 downto 0));
+           led : out STD_LOGIC_VECTOR(7 downto 0));
 end UserController;
 
 architecture Behavioral of UserController is
+
+  component Controller is
+      Port (inputs : in std_logic_vector( 1 downto 0 );     --input values from switches
+          mode : in boolean;
+          output : out std_logic_vector( 7 downto 0 ));   --output result for LEDs
+  end component;
+
+  signal outputs : std_logic_vector (7 downto 0);
+  signal mode : boolean;
+  signal inputs : std_logic_vector (1 downto 0);
 begin
   --mode select
+  process (sw(7))
+      variable switch : std_logic;
+    begin
+      switch := sw(7);
+      if switch = '0' then
+        mode <= false;
+      else
+        mode <= true;
+      end if;
+  end process;
   
   --display select
   process (sw(6))
+    variable switch : std_logic;
     begin
-      if sw(6) = 1 then
+      switch := sw(6);
+      if switch = '1' then
+        --display H/L value
         led(0)<='0';
       	 led(3)<='0';
       	 led(4)<='0';
@@ -49,6 +72,7 @@ begin
 				  led(2)<='1';
 			  end if;
       else
+        --display bits
         led(0)<=outputs(0);
         led(1)<=outputs(1);
 	      led(2)<=outputs(2);
@@ -62,8 +86,9 @@ begin
   --training controls
   
   --inputs
-  process(sw(1),sw(0))
-    begin
-      
-  end process;
+  inputs <= sw(1 downto 0);
+  
+  --instantiate components
+  ctrlr: Controller
+      port map(inputs, mode, outputs);
 end Behavioral;
