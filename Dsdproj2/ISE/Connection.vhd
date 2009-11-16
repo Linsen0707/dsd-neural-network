ce696a3996250bb2d5e1cfa6c2cdd0bec5e9c58a
subquantum <subquantum@478d732c-b2ee-11de-bed4-89e45268dd3d>
2009-11-16 20:25:55 +0000

    Added comments. Fixed Neuron.vhd]

--- Dsdproj2/ISE/Connection.vhd
+++ Dsdproj2/ISE/Connection.vhd
@@ -1,14 +1,15 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: RIT 
+-- Engineer: Alex Karantza
 -- 
--- Create Date:    17:43:49 10/19/2009 
--- Design Name: 
+-- Create Date:    17:59:52 10/19/2009 
+-- Design Name: 	 DSD Project
 -- Module Name:    Connection - Behavioral 
--- Project Name: 
--- Target Devices: 
+-- Project Name:   DSD Project
+-- Target Devices: Spartan 3e-100
 -- Tool versions: 
--- Description: 
+-- Description:    Modulates an input signal by an internal weight, and can
+--                 modify that internal weight based on an error signal
 --
 -- Dependencies: 
 --
@@ -20,7 +21,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ---- Uncomment the following library declaration if instantiating
 ---- any Xilinx primitives in this code.
@@ -28,41 +28,45 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity Connection is
-    Port ( learnmode : in boolean;
-           iv : in unsigned(7 downto 0);
-           ov : out signed(15 downto 0);
-           ie : in signed(15 downto 0);
-           oe : out signed(15 downto 0);
-           iw : in signed(15 downto 0));
+    Port ( learnmode : in boolean;        -- Training or using
+           iv : in unsigned(7 downto 0);  -- Input signal
+           ov : out signed(15 downto 0);  -- Output signal
+           ie : in signed(15 downto 0);   -- Input error
+           oe : out signed(15 downto 0);  -- Output error
+           iw : in signed(15 downto 0));  -- Original weight
 end Connection;
 
 architecture Behavioral of Connection is
 
-signal w : signed(15 downto 0);
-signal trained : boolean := false;
+  -- Internal weight
+  signal w : signed(15 downto 0);
+  -- Use the internal weight, or the input weight?
+  signal trained : boolean := false;
 
 begin
-  
+  -- Use our trained weight after the first training session
   trained <= trained or learnmode;
   process(iv, ie)
+	 -- Intermediate used in 16 bit multiplication
     variable inter : signed(31 downto 0);
+	 -- The new weight to calculate
     variable nw : signed(15 downto 0);
   begin
     case learnmode is
-    when false => 
+    when false => -- Not being trained
       if (not trained) then 
-        nw := iw;
+        nw := iw; -- Use input weight
       else
-        nw := w;
+        nw := w;  -- Use internal weight
       end if;
-      inter := (signed("00000000" & iv) * nw);
-      ov <= inter(23 downto 8);
-      w <= nw;
+      inter := (signed("00000000" & iv) * nw); -- Modulate the signal
+      ov <= inter(23 downto 8); -- Crop out the requisite bits from the multiplication
+      w <= nw; -- Set up the new stored weight
     when true =>
-      inter := ie * w;
-      oe <= inter(23 downto 8);
-      inter := (ie * signed("00000000" & iv));
-      w <= w + inter(24 downto 9);
+      inter := ie * w; -- Modulate the error signal
+      oe <= inter(23 downto 8); -- Crop out the right bits
+      inter := (ie * signed("00000000" & iv)); -- Find the adjustment factor
+      w <= w + inter(24 downto 9); -- Adjust the stored weight by 1/2 of the adjustment factor
     end case;
   end process;
 	
--- Dsdproj2/ISE/Neuron.vhd
+++ Dsdproj2/ISE/Neuron.vhd
@@ -1,14 +1,15 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: RIT 
+-- Engineer: Alex Karantza
 -- 
--- Create Date:    14:59:30 10/19/2009 
--- Design Name: 
--- Module Name:    Neuron - Behavioral 
--- Project Name: 
--- Target Devices: 
+-- Create Date:    17:59:52 10/19/2009 
+-- Design Name: 	 DSD Project
+-- Module Name:    Neuron - Mixed 
+-- Project Name:   DSD Project
+-- Target Devices: Spartan 3e-100
 -- Tool versions: 
--- Description: 
+-- Description:    Accumulates the input signals and sigmoids;
+--                 Accumulates the error signals and modulates by the gaussian.
 --
 -- Dependencies: 
 --
@@ -20,58 +21,64 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ---- Uncomment the following library declaration if instantiating
 ---- any Xilinx primitives in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
+
 entity Neuron is
 	Port (learnmode : in boolean;
-       ia : in signed(15 downto 0);
-    	  ib : in signed(15 downto 0);
-    		 ic : in signed(15 downto 0);
-  		   ea : in signed(15 downto 0);
-       eb : in signed(15 downto 0);
-       ec : in signed(15 downto 0);
-       outerror : out signed(15 downto 0);  
-			 outvalue : out unsigned(7 downto 0));
+       ia : in signed(15 downto 0); -- A Input
+    	 ib : in signed(15 downto 0); -- B Input
+       ic : in signed(15 downto 0); -- C Input 
+  		 ea : in signed(15 downto 0); -- A Error
+       eb : in signed(15 downto 0); -- B Error
+       ec : in signed(15 downto 0); -- C Error
+       outerror : out signed(15 downto 0);  -- Output error
+       outvalue : out unsigned(7 downto 0) ); -- Output signal
 end Neuron;
 
 
 
 architecture Mixed of Neuron is
     
-
+  -- Sum of input signals
   signal sum : signed (15 downto 0):="0000000000000000";
-	signal errsum : signed (31 downto 0);
-	signal dvalue : signed (31 downto 0);
-	signal lastvalue,output : unsigned(7 downto 0);
+
+  -- Sum of error signals
+  signal errsum : signed (31 downto 0);
+
+  -- Derivative-sigmoid of previous
+  signal dvalue : signed (31 downto 0);
+
+  -- Last signal output
+  signal lastvalue : unsigned(7 downto 0);
 	
+  -- Uses the sigmoid
   component Sigmoid is
     Port ( X : in  signed(15 downto 0);
            Y : out  unsigned(7 downto 0));
    end component;
 
+	
 begin
-  
+
+  -- Sum of the A,B,C signal inputs
   sum <= ia + ib + ic;
-  
+  -- Pass that sum through the sigmoid
   sigmoider: Sigmoid
     port map(sum, lastvalue);
-
-  process(lastvalue)
-  begin
-    if lastvalue'EVENT and lastvalue /= output then
-      output <= lastvalue;
-      outvalue <= output;
+  -- And output it
+  outvalue <= lastvalue;
     
-      dvalue <= signed(("0000000100000000" - ("00000000"&lastvalue)) * ("00000000"&lastvalue));
-      errsum <= (ea + eb + ec) * dvalue(23 downto 8);
-     	outerror <= errsum(23 downto 8);
-   	end if;
- 	end process;
+  -- Compute the gaussian
+  dvalue <= signed(("0000000100000000" - ("00000000"&lastvalue)) * ("00000000"&lastvalue));
+  -- The error sum modulated by the gaussian
+  errsum <= (ea + eb + ec) * dvalue(23 downto 8);
+  -- Output the correct bits
+  outerror <= errsum(23 downto 8);
  
 end Mixed;
 
